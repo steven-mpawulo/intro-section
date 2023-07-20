ad6acbb98c1460a2ca3c1c2d6fd9f485e9461e28
steven-mpawulo <stevenmpawulo@gmail.com>
2023-07-20 15:30:25 +0300

    adds logo

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../images/logo.svg'
+
+const Header = () => {
+  return (
+    <header>
+        <img src={Logo} alt="" />
+    </header>
+  )
+}
+
+export default Header
--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from './Header'
+import Main from './Main'
 
 const IntroSection = () => {
   return (
     <>
+    <Header/>
+    <Main/>
     </>
   )
 }
