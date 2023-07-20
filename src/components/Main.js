650b4a5d85ade08bcbcdb74dba5c8c8a14efcab2
steven-mpawulo <stevenmpawulo@gmail.com>
2023-07-20 17:20:56 +0300

    writes html for main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import Databiz from '../images/client-databiz.svg'
+import Audiophile from '../images/client-audiophile.svg'
+import Meet from '../images/client-meet.svg'
+import Maker from '../images/client-maker.svg'
+import Hero from '../images/image-hero-desktop.png'
 
 const Main = () => {
   return (
-    <div></div>
+    <main>
+        <div className="main-container">
+            <div className="first-container">
+                <p>Make</p>
+                <p>Remote work</p>
+                <p>Get your team in sync, no matter your location</p>
+                <p>Streamline processes, create team rituals, and watch productivity soar.</p>
+                <button className='learn-more-button'>Learn more</button>
+                <div className="my-images">
+                    <img src={Databiz} alt="databiz" />
+                    <img src={Audiophile} alt="" />
+                    <img src={Meet} alt="" />
+                    <img src={Maker} alt="" />
+                </div>
+            </div>
+            <div className="second-container">
+                <img src={Hero} alt="" />
+            </div>
+        </div>
+    </main>
   )
 }
 
